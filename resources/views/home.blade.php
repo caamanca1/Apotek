e8c99b217b34174989ea56bc954c2b281f3de6ba
lubnasalsabila <sitilubnasalsabilamuslimah@smkwikrama.sch.id>
2023-12-04 15:14:29 +0700

    Finish Study Case #1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.template')
+
+@section('content')
+@if (Session::get('success'))
+    <div class="alert alert-success">{{ Session::get('success') }}</div>
+@endif
+@if (Session::get('LoggedIn'))
+    <div class="alert alert-danger">{{ Session::get('LoggedIn') }}</div>
+@endif
+<div class="jumbotron py-4 px-5">
+    <h1 class="display-4">
+        Selamat Datang {{ Auth::user()->name }}
+    </h1>
+    <hr class="my-4">
+    <p>Aplikasi ini digunakan oleh pegawai apotek sebagai alat untuk mengelola obat, penyetokan, juga pembelian (kasir)</p>
+</div>
+@endsection
